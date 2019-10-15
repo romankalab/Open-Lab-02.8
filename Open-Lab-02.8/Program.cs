96ed7b3e2efaaea32c6ddbca3ba8f71bcf27ffd2
romankalab <roan.kalab@mail.com>
2019-10-15 21:58:10 +0200

    Initial

---
+++ Open-Lab-02.8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Open_Lab_02._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string word = Console.ReadLine();
+
+            bool result = StringEqual(word);
+            Console.WriteLine($"{result}");
+        }
+
+        private static bool StringEqual(string word)
+        {
+            return word == ("");
+        }
+    }
+}
